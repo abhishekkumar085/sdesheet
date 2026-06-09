9c8eb8ff39e66e0e3aadaf7776f1f35687dd328d
Abhishek Kumar <abhishekkumar08528@gmail.com>
2026-06-09 09:30:42 +0530

    Added Optimal approach using flatten array in 1d and then applied binary search

--- Day5/SearchIn2DMatrix.java
+++ Day5/SearchIn2DMatrix.java
@@ -16,6 +16,79 @@ public class SearchIn2DMatrix {
 
     }
 
+    // Better Approach using Binary Search
+
+    public static boolean binarySearch(int[] nums, int target) {
+        int n = nums.length;
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            // calculate mid;
+
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return true;
+
+            // If target is greater, search in the right half
+            else if (target > nums[mid]) {
+                low = mid + 1;
+            }
+
+            else
+                high = mid - 1;
+        }
+
+        return false;
+    }
+
+    public static boolean searchIn2dMatrixBetter(int[][] mat, int target) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        for (int i = 0; i < n; i++) {
+            if (mat[i][0] <= target && target <= mat[i][m - 1])
+                return binarySearch(mat[i], target);
+
+        }
+
+        return false;
+
+    }
+
+    // Time Complexity O(n*logm)
+    // SC O(1)
+
+    // OPTIMAL APPROACH
+    // Function to search target in 2D matrix using binary search
+    public boolean searchIn2DMatrixOptimal(int[][] mat, int target) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int low = 0;
+        int high = n * m - 1;
+
+        while (low <= high) {
+            // calculate mid
+
+            int mid = (low + high) / 2;
+
+            // Map 1d index to 2d coordinates
+            int row = mid / m;
+            int col = mid % m;
+
+            if (mat[row][col] == target) {
+                return true;
+            } else if (mat[row][col] < target) {
+                low = mid + 1;
+            } else
+                high = mid - 1;
+
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[][] mat = {
                 { 1, 2, 3, 4 },
@@ -28,4 +101,7 @@ public class SearchIn2DMatrix {
         boolean result = searchIn2dMatrix(mat, target);
         System.out.print(result);
     }
+
+    // The TC of O(n*m)
+    // The SC of O(1)
 }
