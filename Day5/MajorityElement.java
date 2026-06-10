f01473e9389d27e0b6899e3bb58c388695c4d56f
Abhishek Kumar <abhishekkumar08528@gmail.com>
2026-06-10 09:46:11 +0530

    Added hashmap

--- Day5/MajorityElement.java
+++ Day5/MajorityElement.java
@@ -1,5 +1,7 @@
 package Day5;
 
+import java.util.HashMap;
+
 public class MajorityElement {
     public static int majorityElement(int[] nums) {
         int n = nums.length;
@@ -17,9 +19,32 @@ public class MajorityElement {
         return -1;
     }
 
+    // using Hashmap
+    public static int majorityElementUsingHashmap(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) {
+                int frequency = map.get(nums[i]);
+                map.put(nums[i], frequency + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+            if (map.get(nums[i]) > n / 2) {
+                return nums[i];
+
+            }
+        }
+
+        System.out.println(map);
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] nums = { 2, 2, 3, 4, 4, 4, 4, 4, 4, 4, 1, 2, 3, 4, 5 };
-        int result = majorityElement(nums);
+        int result = majorityElementUsingHashmap(nums);
         System.out.println(result);
+
     }
 }
